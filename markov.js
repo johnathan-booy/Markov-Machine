8fac2c50ef882b666a018c7c28dd49da37c22793
Johnathan Booy <johnny_music@icloud.com>
2022-11-01 13:53:20 -0700

    Start on sentence beginnings and end on periods.

--- markov.js
+++ markov.js
@@ -7,35 +7,60 @@ class MarkovMachine {
 	/** build markov machine; read in text.*/
 
 	constructor(text) {
+		// Get keys that start lines
+		const startKeys = new Set();
+		let lines = text.split("\n");
+		lines = lines.filter((line) => line !== "");
+		for (const line of lines) {
+			const words = line.split(/[ \r\n]+/);
+			if (words.length > 1) {
+				startKeys.add(`${words[0]} ${words[1]}`);
+			}
+		}
+		this.startKeys = Array.from(startKeys);
+
+		// Get all words
 		let words = text.split(/[ \r\n]+/);
 		this.words = words.filter((c) => c !== "");
+
+		// Analyze Markov Chains
 		this.chains = this.makeChains();
 	}
 
 	makeChains() {
 		const chains = new Map();
-		this.words.forEach((word, idx, arr) => {
-			const nextWord = arr[idx + 1] || null;
-			if (chains.get(word)) {
-				chains.get(word).push(nextWord);
+
+		for (let i = 0; i < this.words.length - 1; i++) {
+			const arr = this.words;
+			const bigram = `${arr[i]} ${arr[i + 1]}`;
+			const nextWord = arr[i + 2] || null;
+			if (chains.get(bigram)) {
+				chains.get(bigram).push(nextWord);
 			} else {
-				chains.set(word, [nextWord]);
+				chains.set(bigram, [nextWord]);
 			}
-		});
+		}
 		return chains;
 	}
 
 	/** return random text from chains */
 
 	makeText(numWords = 100) {
-		const out = [];
 		const allKeys = Array.from(this.chains.keys());
-		let key = getRandomElement(allKeys);
+		let key = getRandomElement(this.startKeys);
+		console.log("Key:", key);
+		const out = [];
 		while (key !== null && out.length < numWords) {
-			out.push(key);
-			key = getRandomElement(this.chains.get(key));
+			let [w1, w2] = key.split(" ");
+			out.push(w1);
+			key = `${w2} ${getRandomElement(this.chains.get(key))}`;
 		}
-		return out.join(" ");
+		let text = out.join(" ");
+		return this.endAtPeriod(text);
+	}
+
+	endAtPeriod(str) {
+		return str.slice(0, str.lastIndexOf(".") + 1);
 	}
 }
 
--- markov.test.js
+++ markov.test.js
@@ -3,11 +3,11 @@ const { MarkovMachine } = require("./markov");
 describe("makeText function", () => {
 	let machine;
 	beforeAll(() => {
-		machine = new MarkovMachine(`the cat in the hat`);
+		machine = new MarkovMachine(`the cat in the hat is in the hat`);
 	});
 	test("should return keys", () => {
-		const res = machine.makeText();
 		console.log(machine.chains);
+		machine.makeText();
 		// expect(machine.keys).toContain("ham.");
 	});
 });
